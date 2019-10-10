442663f3a4bddb60d98613555b3a3e696ab86375
Robert Hubert <robertfhubert@gmail.com>
2019-10-10 12:18:11 -0700

    add selectors for city

--- client/src/components/widgets/weather_current.jsx
+++ client/src/components/widgets/weather_current.jsx
@@ -6,11 +6,21 @@ export default class WeatherCurrent extends Component {
         this.state = {
             isLoaded: false,
             error: null,
-            weather: []
+            weather: [],
+            showMenu: false,
+            menuText: "Choose Location",
+            weatherLocation: "94110",
+            locationText: "San Francisco"
         }
+        this.showMenu = this.showMenu.bind(this);
+        this.chooseSF = this.chooseSF.bind(this);
+        this.chooseDC = this.chooseDC.bind(this);
+        this.chooseNY = this.chooseNY.bind(this);
+        this.chooseLA = this.chooseLA.bind(this);
+        this.chooseCH = this.chooseCH.bind(this);
     }
     componentDidMount(){
-        fetch("http://api.weatherunlocked.com/api/current/us.94110?app_id=e77a1456&app_key=1ed2bd8e094ad579325aba7e1cbbcd0a")
+        fetch(`http://api.weatherunlocked.com/api/current/us.${this.state.weatherLocation}?app_id=e77a1456&app_key=1ed2bd8e094ad579325aba7e1cbbcd0a`)
             .then(res => res.text())
             .then(
                 (result) => {
@@ -28,21 +38,79 @@ export default class WeatherCurrent extends Component {
                     });
                 }
             )
-        // { "lat": 40.78, "lon": -73.97, "alt_m": 47.0, "alt_ft": 154.2, "wx_desc": "Mostly cloudy", "wx_code": 1, "wx_icon": "PartlyCloudyNight.gif", "temp_c": 12.2, "temp_f": 53.96, "feelslike_c": 10.25, "feelslike_f": 50.45, "humid_pct": 74.0, "windspd_mph": 11.81, "windspd_kmh": 19.0, "windspd_kts": 10.26, "windspd_ms": 5.28, "winddir_deg": 40.0, "winddir_compass": "NE", "cloudtotal_pct": 75.0, "vis_km": 16.0, "vis_mi": 9.94, "vis_desc": null, "slp_mb": 1025.4, "slp_in": 30.37, "dewpoint_c": 7.72, "dewpoint_f": 45.9 }
-        // let request = new XMLHttpRequest(),
-        //     method = "GET",
-        //     url = "http://api.weatherunlocked.com/api/current/40.71,-74.00?app_id=e77a1456&app_key=1ed2bd8e094ad579325aba7e1cbbcd0a";
+    
+    }
+    componentDidUpdate(){
+        fetch(`http://api.weatherunlocked.com/api/current/us.${this.state.weatherLocation}?app_id=e77a1456&app_key=1ed2bd8e094ad579325aba7e1cbbcd0a`)
+            .then(res => res.text())
+            .then(
+                (result) => {
+                    // console.log(result)
+                    this.setState({
+                        isLoaded: true,
+                        weather: result
+                    });
+                },
+                
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    
+    }
+    showMenu(event) {
+        event.preventDefault();
+        if (!this.state.showMenu){
+            this.setState({
+                showMenu: true,
+                menuText: "Close"
+            });
+        } else {
+            this.setState({
+                showMenu: false,
+                menuText: "Choose Location"
+            });
+        }
+    }
+    chooseSF(event){
+        event.preventDefault();
+        this.setState({
+            weatherLocation: "94110",
+            locationText: "San Francisco"
+        })
 
-        // request.open(method, url, true);
-        // request.onreadystatechange = function () {
-        //     if (request.readyState === 4 && request.status === 200) {
-        //         // console.log(request.responseText);
-        //         this.setState({
-        //             weather: request.responseText
-        //         })
-        //     }
-        // };
-        // request.send();
+    }
+    chooseDC(event){
+        event.preventDefault();
+        this.setState({
+            weatherLocation: "20001",
+            locationText: "Washington DC"
+        })
+        console.log(this.state.weatherLocation)
+    }
+    chooseNY(event){
+        event.preventDefault();
+        this.setState({
+            weatherLocation: "10001",
+            locationText: "New York City"
+        })
+    }
+    chooseCH(event){
+        event.preventDefault();
+        this.setState({
+            weatherLocation: "60007",
+            locationText: "Chicago"
+        })
+    }
+    chooseLA(event){
+        event.preventDefault();
+        this.setState({
+            weatherLocation: "90001",
+            locationText: "Los Angeles"
+        })
     }
     render() {
         if (this.state.error){
@@ -56,16 +124,30 @@ export default class WeatherCurrent extends Component {
             )
         }
         let weather  = this.state.weather
-        console.log(JSON.parse(weather))
+        
         weather = JSON.parse(weather)
         return (
             <div className="weather-widget">
                 
-                <p>Showing weather in: San Francisco </p>
+                <p>Showing weather in: {this.state.locationText} </p>
                 <img src={weather["wx_icon"]}/>
                 <p>{weather["wx_desc"]}</p>
                 <p>Temperature: {weather["temp_f"]}</p>
                 <p>Feels Like: {weather["feelslike_f"]}</p>
+                <div className="selector">
+                    <button onClick={this.showMenu}>{this.state.menuText}</button>
+                    {
+                        this.state.showMenu ? (
+                            <div classNames="weather-buttons">
+                                <button onClick={this.chooseSF}>San Francisco</button>
+                                <button onClick={this.chooseNY}>New York</button>
+                                <button onClick={this.chooseDC}>Washington DC</button>
+                                <button onClick={this.chooseLA}>Los Angeles</button>
+                                <button onClick={this.chooseCH}>Chicago</button>
+                            </div>
+                        ) : null
+                    }
+                </div>
             </div>
         )
     }
