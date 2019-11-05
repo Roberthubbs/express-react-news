3609f192e85e7757d214c86d2a0902d2c23f7165
Robert Hubert <robertfhubert@gmail.com>
2019-11-05 14:58:18 -0800

    Add comment down button

--- client/src/components/comments/all_comments.jsx
+++ client/src/components/comments/all_comments.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
 class AllComments extends Component {
     constructor(props){
         super(props)
@@ -9,14 +11,15 @@ class AllComments extends Component {
             body: "",
             userId: this.props.userId,
             articleId: this.props.articleId,
-            commentPosted: false
+            commentPosted: false,
+            clickedWide: false,
+            icon: faAngleDown
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCommentClick = this.handleCommentClick.bind(this);
     }
     async componentDidMount(){
-        this.props.fetchAllComments(this.props.articleId).then((res) => {
-            
-        })
+        this.props.fetchAllComments(this.props.articleId);
     }
     componentDidUpdate(prevProps){
         if (this.state.commentPosted){
@@ -36,6 +39,14 @@ class AllComments extends Component {
         this.props.createNewComment(this.state).then(this.props.history.push(`/show/${this.props.articleId}`));
 
     }
+    handleCommentClick(e){
+        e.preventDefault();
+        if (this.state.clickedWide){
+            this.setState({clickedWide: false, icon: faAngleDown})
+        } else {
+            this.setState({clickedWide: true, icon: faAngleUp})
+        }
+    }
     render() {
         const comments = this.props.comments
         
@@ -50,7 +61,7 @@ class AllComments extends Component {
                 <div className="comment-form">
                     <p className="comment-title">Join The Conversation</p>
                     <form action="">
-                        <input type="text"
+                        <input type="textbox"
                             className="comment-input"
                             value={this.state.description}
                             onChange={this.updateField("body")} />
@@ -59,12 +70,15 @@ class AllComments extends Component {
 
                 </div> : null}
                 <p className="article-comments">This article has {comments.length} comments</p>
+                <button onClick={this.handleCommentClick} className="view-comments-button"><FontAwesomeIcon icon={this.state.icon} /></button>
+                {this.state.clickedWide ?
+                <div>
                 {comments.map((comment) => (
                     <div className="comment">
                         <p className="comment-username">Comment by: {comment.username}, {comment.politics} <Link to={`/user/${comment.userId}`}>View User</Link></p>
                         <p>{comment.content}</p>
                     </div>
-                ))}
+                ))}</div>  : null }
                 
             </div>
         )
